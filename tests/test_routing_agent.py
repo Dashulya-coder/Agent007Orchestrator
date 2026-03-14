ad5202f13b0eb7eedf9bb6fbbe6b8be3ac187d98
krkucher <kkucher030807@gmail.com>
2026-03-14 20:26:14 +0200

    added routing test template

---
+++ tests/test_routing_agent.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestRoutingAgent(unittest.TestCase):
+
+    def test_case_1(self):
+        pass
+
+    def test_case_2(self):
+        pass
+
+    def test_case_3(self):
+        pass
+
+    def test_case_4(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
